c95a3f442818e10f9f2654cfe4ad1ab414db5ed3
Reece Crone <reece.crone@gmail.com>
2025-10-21 14:30:28 -0600

    added a note widget

---
+++ app/components/widgets/NoteWidget.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-slate-900 text-white rounded-lg p-1 h-full flex flex-col">
+    <textarea
+      v-model="note"
+      placeholder="Write your note..."
+      class="w-full h-full bg-slate-800 rounded p-2 resize-none focus:outline-none"
+    ></textarea>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const note = ref('')
+</script>
