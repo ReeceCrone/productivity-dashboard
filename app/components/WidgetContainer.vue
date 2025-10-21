1183f22938dd0431ce45393675f62518487bdf17
Reece Crone <reece.crone@gmail.com>
2025-10-21 11:51:34 -0600

    made it so you can atually set the widgets in the grid items and remove grid items

--- app/components/WidgetContainer.vue
+++ app/components/WidgetContainer.vue
@@ -1,52 +1,87 @@
 <template>
   <client-only>
-    <div class="mb-4 flex gap-2">
-      <button @click="addWidget" class="bg-blue-500 text-white px-4 py-2 rounded">
-        Add Widget
-      </button>
-    </div>
-
     <GridLayout
       v-model:layout="layout"
       :col-num="12"
       :row-height="50"
       :is-draggable="true"
       :is-resizable="true"
-      :vertical-compact="false"
       :margin="[10, 10]"
+      :vertical-compact="false"
     >
-      <GridItem v-for="item in layout" :key="item.i" :i="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h">
-        <div class="bg-slate-800 rounded-lg p-4 text-center h-full flex items-center justify-center">
-          {{ item.i.toUpperCase() }}
+      <GridItem
+        v-for="item in layout"
+        :key="item.i"
+        :i="item.i"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+      >
+        <div class="h-full flex flex-col justify-between relative">
+          <!-- Dynamic component rendering -->
+          <component
+            v-if="item.type"
+            :is="getComponent(item.type)"
+            class="h-full"
+          />
+          <div v-else class="bg-slate-800 rounded-lg flex items-center justify-center h-full">
+            Empty
+          </div>
+
+          <!-- Controls -->
+          <div class="absolute top-1 right-1 flex gap-1">
+            <button @click="selectWidgetType(item)" class="bg-blue-500 text-white px-2 py-1 rounded text-xs">
+              Set
+            </button>
+            <button @click="removeWidget(item.i)" class="bg-red-500 text-white px-2 py-1 rounded text-xs">
+              X
+            </button>
+          </div>
         </div>
       </GridItem>
     </GridLayout>
+
+    <button @click="addWidget" class="absolute top-2 right-2 z-10 bg-green-500 text-white px-4 py-2 rounded shadow">
+      Add Widget
+    </button>
   </client-only>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { GridLayout, GridItem } from 'vue3-grid-layout'
+import ClockWidget from './widgets/ClockWidget.vue'
 
 const layout = ref([
-  { i: 'a', x: 0, y: 0, w: 4, h: 3 },
-  { i: 'b', x: 4, y: 0, w: 4, h: 3 },
-  { i: 'c', x: 8, y: 0, w: 4, h: 3 },
-  { x: 2, y: 0, w: 2, h: 4, i: '1'},
+  { i: 'a', x: 0, y: 0, w: 4, h: 3, type: 'ClockWidget' },
+  { i: 'b', x: 4, y: 0, w: 4, h: 3, type: null },
+  { i: 'c', x: 8, y: 0, w: 4, h: 3, type: null }
 ])
 
-// simple counter for unique IDs
-let widgetCounter = 1
+const widgetTypes = ['ClockWidget'] // can expand later
+
+function getComponent(type) {
+  const components = {
+    ClockWidget
+  }
+  return components[type] || null
+}
+
+function selectWidgetType(item) {
+  const type = prompt(`Choose a widget type:\n${widgetTypes.join(', ')}`, item.type || '')
+  if (type && widgetTypes.includes(type)) {
+    item.type = type
+  }
+}
+
+function removeWidget(id) {
+  layout.value = layout.value.filter(item => item.i !== id)
+}
 
 function addWidget() {
-  const newWidget = {
-    i: `widget-${widgetCounter++}`,
-    x: 0,      // default position, you can calculate next free spot
-    y: 0,
-    w: 4,
-    h: 3
-  }
-  layout.value.push(newWidget)
+  const id = Date.now().toString()
+  layout.value.push({ i: id, x: 0, y: 0, w: 4, h: 3, type: null })
 }
 </script>
 
