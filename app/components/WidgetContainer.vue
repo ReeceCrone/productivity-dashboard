a99559c810d2325da2095d997111f2c90292cbe5
Reece Crone <reece.crone@gmail.com>
2025-10-21 11:12:44 -0600

    made the vertical compact false so that the widgets can be placed anywhere, and made it so you can add items to the layout with an add widget button

--- app/components/WidgetContainer.vue
+++ app/components/WidgetContainer.vue
@@ -1,11 +1,18 @@
 <template>
   <client-only>
+    <div class="mb-4 flex gap-2">
+      <button @click="addWidget" class="bg-blue-500 text-white px-4 py-2 rounded">
+        Add Widget
+      </button>
+    </div>
+
     <GridLayout
       v-model:layout="layout"
       :col-num="12"
       :row-height="50"
       :is-draggable="true"
       :is-resizable="true"
+      :vertical-compact="false"
       :margin="[10, 10]"
     >
       <GridItem v-for="item in layout" :key="item.i" :i="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h">
@@ -25,7 +32,22 @@ const layout = ref([
   { i: 'a', x: 0, y: 0, w: 4, h: 3 },
   { i: 'b', x: 4, y: 0, w: 4, h: 3 },
   { i: 'c', x: 8, y: 0, w: 4, h: 3 },
+  { x: 2, y: 0, w: 2, h: 4, i: '1'},
 ])
+
+// simple counter for unique IDs
+let widgetCounter = 1
+
+function addWidget() {
+  const newWidget = {
+    i: `widget-${widgetCounter++}`,
+    x: 0,      // default position, you can calculate next free spot
+    y: 0,
+    w: 4,
+    h: 3
+  }
+  layout.value.push(newWidget)
+}
 </script>
 
 
