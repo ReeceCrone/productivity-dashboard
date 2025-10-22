4678ace28cf66c8aa8121be753a0f32a61e5f63d
Reece Crone <reece.crone@gmail.com>
2025-10-21 21:04:12 -0600

    added ability to name the list

---
+++ app/components/widgets/ToDoWidget.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-slate-900 text-white rounded-lg p-1 h-full flex items-center justify-center font-mono">
+      <textarea
+        placeholder="Write your To-Do list description..."
+        class="w-full h-full bg-slate-800 rounded p-2 resize-none focus:outline-none text-xl text-center"
+      ></textarea>
+    </div>
+</template>
